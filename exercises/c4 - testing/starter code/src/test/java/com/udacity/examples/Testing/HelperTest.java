a24423111e91ad1df15f41fe7a98f1966e6fa4bf
Andy Hayes <ahayes@sofi.org>
2020-12-12 10:38:45 -0700

    first commit

--- exercises/c4 - testing/starter code/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ exercises/c4 - testing/starter code/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -1,8 +1,26 @@
 package com.udacity.examples.Testing;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class HelperTest {
 
-	
+	@Test
+	public void verify_getCount(){
+		List<String> empNames = Arrays.asList("Andy", "Linsey");
+		final long actual = Helper.getCount(empNames);
+		assertEquals(2, actual);
+	}
+
+	@Test
+	public void verify_getMergedList(){
+		List<String> empNames = Arrays.asList("Andy", "Linsey");
+		final String actual = Helper.getMergedList(empNames);
+		assertEquals("Andy, Linsey", actual);
+	}
 }
